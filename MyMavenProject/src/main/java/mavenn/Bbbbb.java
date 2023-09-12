2d69ddad63d6cc7827531d04b9616b8b10a3abb2
Dell <Dell@DESKTOP-SETA581>
2023-09-12 21:53:03 +0530

    running

---
+++ MyMavenProject/src/main/java/mavenn/Bbbbb.java
@@ -0,0 +1,9 @@
+package mavenn;
+
+public class Bbbbb {
+	public void run()
+	 {
+System.out.print("Running");
+	}
+
+}
---
+++ MyMavenProject/src/main/java/mavenn/Cccc.java
@@ -0,0 +1,14 @@
+package mavenn;
+
+public class Cccc extends Bbbbb {
+	public void jump()
+	{
+		System.out.println("Animal");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Cccc obj=new Cccc();
+obj.jump();
+obj.run();
+	}
+}
