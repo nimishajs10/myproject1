3ca5662b26cb0f13752f56f006cf2a10bacb1b18
Dell <Dell@DESKTOP-SETA581>
2023-09-12 22:03:57 +0530

    fruitsss

---
+++ MyMavenProject/src/main/java/mavenn/Fruits.java
@@ -0,0 +1,8 @@
+package mavenn;
+
+public class Fruits {
+	public void sweet()
+	{
+	System.out.println("RAW    ");
+	}
+}
---
+++ MyMavenProject/src/main/java/mavenn/Mango.java
@@ -0,0 +1,14 @@
+package mavenn;
+public class Mango extends Fruits {
+	public void raw()
+	{
+		System.out.println("FRUITSS  ");
+	}
+		public static void main(String[] args)
+		{
+			// TODO Auto-generated method stub
+	Mango obj=new Mango();
+	obj.raw();
+	obj.sweet();
+	}
+}
