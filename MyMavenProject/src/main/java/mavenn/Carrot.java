8e84d07fdf350cf46891515a6cc16418a86d68ed
Dell <Dell@DESKTOP-SETA581>
2023-09-12 22:11:54 +0530

    vegetablesss

---
+++ MyMavenProject/src/main/java/mavenn/Carrot.java
@@ -0,0 +1,15 @@
+package mavenn;
+
+public class Carrot extends Veggies {
+	public void ripe()
+	{
+		System.out.println("Ripeee  ");
+	}
+		public static void main(String[] args) 
+		{
+			// TODO Auto-generated method stub
+	Carrot obj=new Carrot();
+	obj.ripe();
+	obj.raw();
+		}
+	}
---
+++ MyMavenProject/src/main/java/mavenn/Veggies.java
@@ -0,0 +1,9 @@
+package mavenn;
+
+public class Veggies {
+	public void raw();
+	{
+		System.out.println("RAWWW ");
+	}
+}
+
